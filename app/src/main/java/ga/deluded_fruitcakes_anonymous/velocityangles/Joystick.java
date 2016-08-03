11a30982ae9ca6d86d28f122289142a3b493d04d
Isaac Goodman <isaactechgenius@isaacstechblog.com>
2016-08-03 09:30:54 -0700

    Update Joystick.java

--- app/src/main/java/ga/deluded_fruitcakes_anonymous/velocityangles/Joystick.java
+++ app/src/main/java/ga/deluded_fruitcakes_anonymous/velocityangles/Joystick.java
@@ -4,4 +4,18 @@ package ga.deluded_fruitcakes_anonymous.velocityangles;
  * Created by Isaac on 8/3/2016.
  */
 public class Square {
+  @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        // MotionEvent reports input details from the touch screen
+        // and other input controls. In this case, you are only
+        // interested in events where the touch position changed.
+
+        float x = e.getX();
+        float y = e.getY();
+
+        switch (e.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Toast.makeText(getContext(), "Screen Touched at " + x + ", " + y, Toast.LENGTH_SHORT).show();
+
+        }
 }
