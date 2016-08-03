811556301f5e056bc3037248a0b64fc46eecad34
Isaac Goodman <isaactechgenius@isaacstechblog.com>
2016-08-03 09:40:24 -0700

    Update MainActivity.java

--- app/src/main/java/ga/deluded_fruitcakes_anonymous/velocityangles/MainActivity.java
+++ app/src/main/java/ga/deluded_fruitcakes_anonymous/velocityangles/MainActivity.java
@@ -1,14 +1,36 @@
-package ga.deluded_fruitcakes_anonymous.velocityangles;
+package net.iplustech.mytoast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
+    MyView v;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        v = new MyView(this);
+        setContentView(v);
     }
-    //I am finding the equation
+    public void explode(View v){
+        //0.0
+        EditText t_inputText = (EditText) findViewById(R.id.t_wordInput);
+        boolean validInputs = false;
+        String message = "default message";
+        try {
+            message = t_inputText.getText().toString();
+            validInputs = true;
+        } catch (Exception e) {
+            // invalid inputs stays false.
+        }
+        Monster m = new Monster(message);
+        Toast.makeText(v.getContext(), m.name,Toast.LENGTH_SHORT).show();
+
+    }
+    public void explode2(View v){
+        //0.1
+        Toast.makeText(v.getContext(), "I WILL DO THY TOAST OF THYSELF DUH Now you do Really suck Alot; for duh jhdgjnhavgbiaugfkbsuvgfhgkbhfgvkhvgfdkbvjhgfsbkjhvgfkjhegvfjhgfuhgbkjhfhfbkjhfgbkjhfgbkajhevgbvfauytfbkvjhbfyboieugfbvsliugboiwuy4tiro",
+                Toast.LENGTH_SHORT).show();
+    }
 }
