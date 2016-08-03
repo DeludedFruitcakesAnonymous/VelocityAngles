914876368aa53484f93750fc05d3a57c0eca1c44
Isaac Goodman <isaactechgenius@isaacstechblog.com>
2016-08-03 10:13:32 -0700

    Update GameView.java

--- app/src/main/java/ga/deluded_fruitcakes_anonymous/velocityangles/GameView.java
+++ app/src/main/java/ga/deluded_fruitcakes_anonymous/velocityangles/GameView.java
@@ -9,18 +9,18 @@ import android.widget.Toast;
  * Created by Isaac on 8/3/2016.
  */
 public class GameView extends View {
-     float centerX, centerY
-  float borderHeight;
-  
-   
-  
-    public GameView(Context context,float sHeight, float sWidth) {
-        super(context); 
-        borderHeight = sHeight * 0.7;
-    centerX = sWidth * 0.5;
-    centerY = sHeight * 0.9;
+    float centerX, centerY;
+    float borderHeight;
+
+
+    public GameView(Context context, float sHeight, float sWidth) {
+        super(context);
+        borderHeight = sHeight * 0.7f;
+        centerX = sWidth * 0.5f;
+        centerY = sHeight * 0.9f;
     }
-     @Override
+
+    @Override
     public boolean onTouchEvent(MotionEvent e) {
         // MotionEvent reports input details from the touch screen
         // and other input controls. In this case, you are only
@@ -28,10 +28,13 @@ public class GameView extends View {
 
         float x = e.getX();
         float y = e.getY();
-    if(y >= borderHeight){
-        switch (e.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                Toast.makeText(getContext(), "Screen Touched at " + (x-centerX) + ", " + (y-centerY), Toast.LENGTH_SHORT).show();
+        if (y >= borderHeight) {
+            switch (e.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    Toast.makeText(getContext(), "Screen Touched at " + (x - centerX) + ", " + (y - centerY), Toast.LENGTH_SHORT).show();
 
-        }}
+            }
+        }
+        return true;
+    }
 }
